b41a730fa92f091984c2a606036fa0f27e9505d7
allan <asalinas@realnet.com.mx>
2019-05-23 12:40:02 -0500

    Commit desde Fedora

--- lector.py
+++ lector.py
@@ -1,12 +1,12 @@
 import os
 import time
 import pymongo
-import json
+
 cliente = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = cliente["prueba"]
 coleccion = mydb["logs"]
 
-f = open('doc.log','r') #Carga del archivo
+f = open('/var/log/remote/FG/192.168.60.36.log','r') #Carga del archivo
 
 def follow(f): #Funcion que lee el ultimo renglon del archivo, si detecta cambios espera 0.3 segundos para volver a correr
     f.seek(0, os.SEEK_END)
--- pruebas.py
+++ pruebas.py
@@ -1,25 +1,48 @@
 import os
 import time
+import re
 import pymongo
 import ast
 import json
-cliente = pymongo.MongoClient("mongodb://localhost:27017/")
-mydb = cliente["prueba"]
-coleccion = mydb["logs2"]
 
-f = open('doc.log','r') #Carga del archivo
+cliente = pymongo.MongoClient("mongodb://asalinas:RealNet2019@localhost:27017/")
+mydb = cliente["prueba"]
+coleccion = mydb["logsprueba"]
+
+f = open('/var/log/remote/FG/192.168.60.36.log','r') #Carga del archivo
 
 def follow(f): #Funcion que lee el ultimo renglon del archivo, si detecta cambios espera 0.3 segundos para volver a correr
     f.seek(0, os.SEEK_END)
     while True:
         line = f.readline()
         if not line:
-            time.sleep(0.3)
+            #time.sleep(0.1)
             continue
         yield line
 
+
 loglines = follow(f)
+
+
 for data in loglines:
+    errores = open('errores.txt','a')
+    errores.write(data)
+    errores.close()
+    s  = re.findall('"(.*?)"',data)
+    for i in range(len(s)):
+        texto = s[i].replace(",",".")
+        data = data.replace(s[i],texto)
+
+    s = re.findall('"(.*?)"', data)
+    for i in range(len(s)):
+        texto = s[i].replace(":", " ")
+        data = data.replace(s[i], texto)
+
+    s = re.findall('"(.*?)"', data)
+    for i in range(len(s)):
+        texto = s[i].replace("=", "==")
+        data = data.replace(s[i], texto)
+
     data = data.replace(" ",",",4)
     data = data.replace('"','')
     data = data.split(",")
@@ -36,3 +59,6 @@ for data in loglines:
         vcast = ast.literal_eval(valor)
         diccionario.update(vcast)
     insercion = coleccion.insert_one(diccionario)
+f.close()
+
+#@
